ea542dc81841f4578b17d5cacb0c1a1597115059
Jasper Linsen <jasper@somethinghere.net>
2020-10-02 07:24:14 -0700

    Added loading more content, refined styles and scripts

--- blog.ts
+++ blog.ts
@@ -1,16 +1,48 @@
+import { delay } from "./ts/general";
 import "./ts/page";
 
-document.querySelectorAll( 'pre, code' ).forEach(code => {
+function initBlogPage( rootElement:HTMLElement ){
 
-    code.innerHTML = code.textContent.replace( /return|var|const|let|function|\=\>|if|else|document|window|async|\;|\{|\}/gi, keyword => {
+    rootElement.querySelectorAll( 'pre, code' ).forEach(code => {
 
-        return `<span class="keyword" style="animation-delay:-${(Math.random()*2).toFixed(2)}s">${keyword}</span>`;
+        code.innerHTML = code.textContent.replace( /return|var|const|let|function|\=\>|if|else|document|window|async|\;|\{|\}/gi, keyword => {
+
+            return `<span class="keyword" style="animation-delay:-${(Math.random()*2).toFixed(2)}s">${keyword}</span>`;
+
+        });
+
+    });
+    rootElement.querySelectorAll( 'img' ).forEach(image => {
+
+        image.addEventListener( 'click', event => image.classList.toggle( 'expanded' ));
+
+    });
+    rootElement.querySelectorAll( '.load-more' ).forEach(loadMonth => {
+
+        loadMonth.addEventListener( 'click', async event => {
+
+            event.preventDefault();
+
+            loadMonth.classList.add( 'loading' );
+
+            await delay(2000);
+
+            const page = await fetch( loadMonth.getAttribute( 'href' ) );
+            const html = await page.text();
+            const dom = new DOMParser().parseFromString( html, 'text/html' );
+            const main = dom.querySelector( 'main' );
+            
+            initBlogPage( main );
+
+            rootElement.parentNode.insertBefore( main, rootElement );
+            rootElement.parentNode.insertBefore( rootElement, main );
+
+            loadMonth.remove();
+
+        }, { once: true });
 
     });
 
-});
-document.querySelectorAll( 'img' ).forEach(image => {
+}
 
-    image.addEventListener( 'click', event => image.classList.toggle( 'expanded' ));
-
-});
+initBlogPage( document.querySelector( 'main' ) );
